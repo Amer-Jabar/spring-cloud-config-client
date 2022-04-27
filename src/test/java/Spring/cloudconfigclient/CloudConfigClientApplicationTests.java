1714061987023337e5eae454756c8a1567209dcb
Amer-Jabar <amerjabar0000@gmail.com>
2022-04-27 21:21:04 +0300

    Initialized repo for spring cloud configs client

---
+++ src/test/java/Spring/cloudconfigclient/CloudConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.cloudconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
